53ee9a461f54053d3815867f65635924fc37e7e2
Samir <samir.valiyev@gmail.com>
2021-03-17 18:37:36 +0400

    added three blogs

--- src/Home.js
+++ src/Home.js
@@ -2,22 +2,24 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(31);
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
 
-    const handleClick = (e) => {
-        setName('Luigi');
-        setAge(23);
-
-    };
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by{blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
 
 export default Home;
+
